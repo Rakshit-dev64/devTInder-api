5e2835b2f1bdb5d5a0e43f10be5d7c523b2d5455
Rakshit-dev64 <rakshityagi2103@gmail.com>
2025-05-26 12:42:06 +0530

    Playing With Routes

--- src/app.js
+++ src/app.js
@@ -2,16 +2,9 @@ const express = require("express");
 
 const app = express();
 
-
-app.get("/user",(req,res)=>{
-    res.send({firstname:"Rakshit", lastname:"Tyagi"})
-})
-app.post("/user",(req,res)=>{
-    res.send("Updated Successfully");
-})
-
-app.delete("/user",(req,res)=>{
-    res.send("User Deleted");
+app.get("/user/:userId/:name",(req,res)=>{
+    console.log(req.params);
+    res.send({firstname : "Rakshit", lastname : "Tyagi"})
 })
 
 app.listen(3000,()=>{
