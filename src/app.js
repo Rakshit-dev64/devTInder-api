da730f56dabd5337771f757dee9bad9eba059ad7
Rakshit-dev64 <rakshityagi2103@gmail.com>
2025-05-26 14:14:59 +0530

    Multiple Route Handlers and Next()

--- src/app.js
+++ src/app.js
@@ -2,11 +2,28 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user/:userId/:name",(req,res)=>{
-    console.log(req.params);
-    res.send({firstname : "Rakshit", lastname : "Tyagi"})
-})
+app.use(
+  "/user",(req, res, next) => {
+    console.log("1st RH");
+    // res.send("1st Response");
+    next();
+  },
+  (req, res, next) => {
+    console.log("2nd RH");
+    // res.send("2nd Response");
+    next();
+  },
+  (req, res, next)=>{
+    console.log("3rd RH");
+    // res.send("3rd Response");
+    next();
+  },
+  (req, res, next)=>{
+    console.log("4th RH");
+    res.send("4th Response");
+  }
+); 
 
-app.listen(3000,()=>{
-    console.log("Server started");
+app.listen(3000, () => {
+  console.log("Server started");
 });
